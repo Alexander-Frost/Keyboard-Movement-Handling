e8f0b8ab9a719718cd00a9ea94f773acbeb4a521
Alex <alexcc17@gmail.com>
2019-04-14 18:55:06 -0400

    ...

--- Sticking Above Keyboard/ViewController.swift
+++ Sticking Above Keyboard/ViewController.swift
@@ -10,11 +10,64 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let btn: UIButton = UIButton(frame: CGRect(x: 100, y: 400, width: 100, height: 50))
+    var activeField: UITextField?
+    var lastOffset: CGPoint!
+    var keyboardHeight: CGFloat!
+
+    @IBOutlet weak var myTextfield: UITextField!
+    
+    @IBOutlet weak var myTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var myBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.hideKeyboardWhenTappedAround()
+
+        // uncomment to use input view
+        // set custom auto word suggestion using this
+//        let customView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 44))
+//        customView.backgroundColor = UIColor.red
+//        myTextfield.inputAccessoryView = customView
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardNotification), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardNotification), name: UIResponder.keyboardWillHideNotification, object: nil)
+    
+        
+        myBtn.backgroundColor = UIColor.green
+        myBtn.setTitle("Click Me", for: .normal)
     }
 
 
+    
+    @objc func handleKeyboardNotification(_ notification: Notification) {
+        
+        if let userInfo = notification.userInfo {
+            
+            let keyboardFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as AnyObject).cgRectValue
+            
+            // if keyboard is showing then increase padding / decrease when hiding
+            if notification.name == UIResponder.keyboardWillShowNotification{
+                myTopConstraint?.constant = -keyboardFrame!.height
+            } else {
+                myTopConstraint?.constant =  +keyboardFrame!.height
+            }
+            UIView.animate(withDuration: 0.5, animations: { () -> Void in
+                self.view.layoutIfNeeded()
+            })
+        }
+    }
 }
 
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+        
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
